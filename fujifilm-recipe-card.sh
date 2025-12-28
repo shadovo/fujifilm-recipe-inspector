98e4e15b43281083d6ab4932932f3a20891d92f0
Oscar Rundh <1194795+shadovo@users.noreply.github.com>
2025-12-28 19:37:22 +0100

    format fixes

--- fujifilm-recipe-card.sh
+++ fujifilm-recipe-card.sh
@@ -52,33 +52,67 @@ font_family=$(
 while IFS=: read -r key value; do
 	value="${value#"${value%%[![:space:]]*}"}"
 	case "$key" in
-	FilmMode) exif_film_mode="$value" ;;
-	HighlightTone) exif_highlight_tone="$value" ;;
-	ShadowTone) exif_shadow_tone="$value" ;;
-	WhiteBalance) exif_white_balance="$value" ;;
-	WhiteBalanceFineTune) exif_white_balance_fine_tune="$value" ;;
-	Saturation) exif_saturation="$value" ;;
-	Sharpness) exif_sharpness="$value" ;;
-	NoiseReduction) exif_noise_reduction="$value" ;;
-	DynamicRangeSetting) exif_dynamic_range_setting="$value" ;;
+	BWAdjustment) exif_bw_adjustment="$value" ;;
+	BWMagentaGreen) exif_bw_magenta_green="$value" ;;
+	Clarity) exif_clarity="$value" ;;
+	ColorChromeEffect) exif_color_chrome_effect="$value" ;;
+	ColorChromeFXBlue) exif_color_chrome_fx_blue="$value" ;;
+	ColorTemperature) exif_color_temperature="$value" ;;
+	DRangePriorityAuto) exif_d_range_priority_auto="$value" ;;
 	DevelopmentDynamicRange) exif_development_dynamic_range="$value" ;;
+	DynamicRangeSetting) exif_dynamic_range_setting="$value" ;;
+	FilmMode) exif_film_mode="$value" ;;
 	GrainEffectRoughness) exif_grain_effect_roughness="$value" ;;
 	GrainEffectSize) exif_grain_effect_size="$value" ;;
-	ColorChromeEffect) exif_color_chrome_effect="$value" ;;
-	ColorChromeFXBlue) exif_color_chrome_xf_blue="$value" ;;
-	Clarity) exif_clarity="$value" ;;
-	ColorTemperature) exif_color_temperature="$value" ;;
+	HighlightTone) exif_highlight_tone="$value" ;;
+	NoiseReduction) exif_noise_reduction="$value" ;;
+	Saturation) exif_saturation="$value" ;;
+	ShadowTone) exif_shadow_tone="$value" ;;
+	Sharpness) exif_sharpness="$value" ;;
+	WhiteBalance) exif_white_balance="$value" ;;
+	WhiteBalanceFineTune) exif_white_balance_fine_tune="$value" ;;
 	esac
 done <<EOF
 $(
 	exiftool -S \
-		-FilmMode -HighlightTone -ShadowTone -WhiteBalance -WhiteBalanceFineTune -Saturation \
-		-Sharpness -NoiseReduction -DynamicRangeSetting -DevelopmentDynamicRange -GrainEffectRoughness -GrainEffectSize \
-		-ColorChromeEffect -ColorChromeFXBlue -Clarity -ColorTemperature \
+		-BWAdjustment \
+		-BWMagentaGreen \
+		-Clarity \
+		-ColorChromeEffect \
+		-ColorChromeFXBlue \
+		-ColorTemperature \
+		-DRangePriorityAuto \
+		-DevelopmentDynamicRange \
+		-DynamicRangeSetting \
+		-FilmMode \
+		-FujiFilm:Sharpness \
+		-GrainEffectRoughness \
+		-GrainEffectSize \
+		-HighlightTone \
+		-NoiseReduction \
+		-Saturation \
+		-ShadowTone \
+		-WhiteBalance \
+		-WhiteBalanceFineTune \
 		"$file_path" |
-		sed -E "s/\(((very|medium) )?(((soft|high|hard|weak)(est)?)|normal)\)//g"
+		sed -E "s/ \(((very|medium) )?(((hard|soft|high|low|strong|weak)(est)?)|normal)\)//g"
 )
 EOF
+
+apply_sign_fix() {
+	local VALUE="$1"
+	local NEW_VALUE=$VALUE
+
+	if [[ "$VALUE" =~ ^[+-]?0$ ]]; then
+		NEW_VALUE="0"
+	elif [[ "$VALUE" =~ ^[+-] ]]; then
+		NEW_VALUE="$VALUE"
+	elif [[ "$VALUE" =~ ^[0-9.] ]]; then
+		NEW_VALUE="+$VALUE"
+	fi
+
+	echo "$NEW_VALUE"
+}
 
 get_film_sim() {
 	local film_sim="${exif_film_mode:-$exif_saturation}"
@@ -119,7 +153,16 @@ get_wb_fine_tune_scaled() {
 	red="${exif_white_balance_fine_tune#* }"
 	red="${red%%,*}"
 	blue="${exif_white_balance_fine_tune##*, Blue }"
-	printf "%+d Red, %+d Blue" $((red / 20)) $((blue / 20))
+	printf "%s Red, %s Blue" "$(apply_sign_fix "$((red / 20))")" "$(apply_sign_fix "$((blue / 20))")"
+}
+
+get_monochromatic_color() {
+	if ((exif_bw_adjustment || exif_bw_magenta_green)); then
+		local wc mg
+		wc="$(apply_sign_fix "${exif_bw_adjustment:-"0"}")"
+		mg="$(apply_sign_fix "${exif_bw_magenta_green:-"0"}")"
+		printf "%s WC, %s MG" "$wc" "$mg"
+	fi
 }
 
 get_white_balance() {
@@ -155,19 +198,29 @@ values+="$(get_white_balance)\n"
 labels+="White Balance Shift:\n"
 values+="$(get_wb_fine_tune_scaled)\n"
 
+monochromatic_color="$(get_monochromatic_color)"
 if [[ ! $exif_saturation =~ .*(Acros|B\&W).* ]]; then
 	labels+="Color:\n"
-	values+="$exif_saturation\n"
+	values+="$(apply_sign_fix "$exif_saturation")\n"
+elif [[ -n $monochromatic_color ]]; then
+	labels+="Monochromatic Color:\n"
+	values+="$monochromatic_color\n"
 fi
 
 labels+="Highlight:\n"
-values+="$exif_highlight_tone\n"
+values+="$(apply_sign_fix "$exif_highlight_tone")\n"
 
 labels+="Shadow:\n"
-values+="$exif_shadow_tone\n"
+values+="$(apply_sign_fix "$exif_shadow_tone")\n"
 
-labels+="Dynamic Range:\n"
-values+="$(get_dynamic_range)\n"
+dynamic_range="$(get_dynamic_range)"
+if [[ -n $dynamic_range ]]; then
+	labels+="Dynamic Range:\n"
+	values+="$dynamic_range\n"
+elif [[ -n $exif_d_range_priority_auto ]]; then
+	labels+="Dynamic Range Priority:\n"
+	values+="$exif_d_range_priority_auto\n"
+fi
 
 labels+="Grain Effect:\n"
 values+="$(get_grain_effect)\n"
@@ -176,13 +229,13 @@ labels+="Color Chrome Effect:\n"
 values+="$exif_color_chrome_effect\n"
 
 labels+="Color Chrome FX Blue:\n"
-values+="$exif_color_chrome_xf_blue\n"
+values+="$exif_color_chrome_fx_blue\n"
 
 labels+="Sharpness:\n"
-values+="$exif_sharpness\n"
+values+="$(apply_sign_fix "$exif_sharpness")\n"
 
 labels+="Clarity:\n"
-values+="$exif_clarity\n"
+values+="$(apply_sign_fix "$exif_clarity")\n"
 
 labels+="Noise Reduction:"
 values+="$exif_noise_reduction"
--- fujifilm-recipe-inspector.sh
+++ fujifilm-recipe-inspector.sh
@@ -27,44 +27,86 @@ box_label_width=25
 while IFS=: read -r key value; do
 	value="${value#"${value%%[![:space:]]*}"}"
 	case "$key" in
-	Make) exif_make="$value" ;;
-	Model) exif_model="$value" ;;
-	LensModel) exif_lens_model="$value" ;;
-	FNumber) exif_f_number="$value" ;;
-	ExposureTime) exif_exposure_time="$value" ;;
-	ISO) exif_iso="$value" ;;
-	ExposureCompensation) exif_exposure_compensation="$value" ;;
-	MeteringMode) exif_metering_mode="$value" ;;
-	FocalLength) exif_focal_length="$value" ;;
-	FilmMode) exif_film_mode="$value" ;;
-	HighlightTone) exif_highlight_tone="$value" ;;
-	ShadowTone) exif_shadow_tone="$value" ;;
-	WhiteBalance) exif_white_balance="$value" ;;
-	WhiteBalanceFineTune) exif_white_balance_fine_tune="$value" ;;
-	Saturation) exif_saturation="$value" ;;
-	Sharpness) exif_sharpness="$value" ;;
-	NoiseReduction) exif_noise_reduction="$value" ;;
-	DynamicRangeSetting) exif_dynamic_range_setting="$value" ;;
-	DevelopmentDynamicRange) exif_development_dynamic_range="$value" ;;
-	GrainEffectRoughness) exif_grain_effect_roughness="$value" ;;
-	GrainEffectSize) exif_grain_effect_size="$value" ;;
+	BWAdjustment) exif_bw_adjustment="$value" ;;
+	BWMagentaGreen) exif_bw_magenta_green="$value" ;;
+	Clarity) exif_clarity="$value" ;;
 	ColorChromeEffect) exif_color_chrome_effect="$value" ;;
 	ColorChromeFXBlue) exif_color_chrome_fx_blue="$value" ;;
-	Clarity) exif_clarity="$value" ;;
 	ColorTemperature) exif_color_temperature="$value" ;;
+	DRangePriorityAuto) exif_d_range_priority_auto="$value" ;;
+	DevelopmentDynamicRange) exif_development_dynamic_range="$value" ;;
+	DynamicRangeSetting) exif_dynamic_range_setting="$value" ;;
+	ExposureCompensation) exif_exposure_compensation="$value" ;;
+	ExposureTime) exif_exposure_time="$value" ;;
+	FNumber) exif_f_number="$value" ;;
+	FilmMode) exif_film_mode="$value" ;;
+	FocalLength) exif_focal_length="$value" ;;
+	GrainEffectRoughness) exif_grain_effect_roughness="$value" ;;
+	GrainEffectSize) exif_grain_effect_size="$value" ;;
+	HighlightTone) exif_highlight_tone="$value" ;;
+	ISO) exif_iso="$value" ;;
+	LensModel) exif_lens_model="$value" ;;
+	Make) exif_make="$value" ;;
+	MeteringMode) exif_metering_mode="$value" ;;
+	Model) exif_model="$value" ;;
+	NoiseReduction) exif_noise_reduction="$value" ;;
+	Saturation) exif_saturation="$value" ;;
+	ShadowTone) exif_shadow_tone="$value" ;;
+	Sharpness) exif_sharpness="$value" ;;
+	WhiteBalance) exif_white_balance="$value" ;;
+	WhiteBalanceFineTune) exif_white_balance_fine_tune="$value" ;;
 	esac
 done <<EOF
 $(
 	exiftool -S \
-		-Make -Model -LensModel \
-		-FNumber -ExposureTime -ISO -ExposureCompensation -MeteringMode -FocalLength \
-		-FilmMode -HighlightTone -ShadowTone -WhiteBalance -WhiteBalanceFineTune -Saturation \
-		-Sharpness -NoiseReduction -DynamicRangeSetting -DevelopmentDynamicRange -GrainEffectRoughness -GrainEffectSize \
-		-ColorChromeEffect -ColorChromeFXBlue -Clarity -ColorTemperature \
+		-BWAdjustment \
+		-BWMagentaGreen \
+		-Clarity \
+		-Clarity \
+		-ColorChromeEffect \
+		-ColorChromeFXBlue \
+		-ColorTemperature \
+		-DRangePriorityAuto \
+		-DevelopmentDynamicRange \
+		-DynamicRangeSetting \
+		-ExposureCompensation \
+		-ExposureTime \
+		-FNumber \
+		-FilmMode \
+		-FocalLength \
+		-FujiFilm:Sharpness \
+		-GrainEffectRoughness \
+		-GrainEffectSize \
+		-HighlightTone \
+		-ISO \
+		-LensModel \
+		-Make \
+		-MeteringMode \
+		-Model \
+		-NoiseReduction \
+		-Saturation \
+		-ShadowTone \
+		-WhiteBalance \
+		-WhiteBalanceFineTune \
 		"$file_path" |
-		sed -E "s/\(((very|medium) )?(((soft|high|hard|weak)(est)?)|normal)\)//g"
+		sed -E "s/ \(((very|medium) )?(((hard|soft|high|low|strong|weak)(est)?)|normal)\)//g"
 )
 EOF
+
+apply_sign_fix() {
+	local VALUE="$1"
+	local NEW_VALUE=$VALUE
+
+	if [[ "$VALUE" =~ ^[+-]?0$ ]]; then
+		NEW_VALUE="0"
+	elif [[ "$VALUE" =~ ^[+-] ]]; then
+		NEW_VALUE="$VALUE"
+	elif [[ "$VALUE" =~ ^[0-9.] ]]; then
+		NEW_VALUE="+$VALUE"
+	fi
+
+	echo "$NEW_VALUE"
+}
 
 get_film_sim() {
 	local film_sim="${exif_film_mode:-$exif_saturation}"
@@ -100,12 +142,21 @@ get_film_sim() {
 	esac
 }
 
-format_wb_fine_tune_scaled() {
+get_wb_fine_tune_scaled() {
 	local red blue
 	red="${exif_white_balance_fine_tune#* }"
 	red="${red%%,*}"
 	blue="${exif_white_balance_fine_tune##*, Blue }"
-	printf "%+d Red, %+d Blue" $((red / 20)) $((blue / 20))
+	printf "%s Red, %s Blue" "$(apply_sign_fix "$((red / 20))")" "$(apply_sign_fix "$((blue / 20))")"
+}
+
+get_monochromatic_color() {
+	if ((exif_bw_adjustment || exif_bw_magenta_green)); then
+		local wc mg
+		wc="$(apply_sign_fix "${exif_bw_adjustment:-"0"}")"
+		mg="$(apply_sign_fix "${exif_bw_magenta_green:-"0"}")"
+		printf "%s WC, %s MG" "$wc" "$mg"
+	fi
 }
 
 get_white_balance() {
@@ -196,17 +247,28 @@ print_section_divider
 print_section_heading_line "Recipe"
 print_data_line "Film Simulation" "$(get_film_sim)"
 print_data_line "White Balance" "$(get_white_balance)"
-print_data_line "WB Shift" "$(format_wb_fine_tune_scaled)"
+print_data_line "WB Shift" "$(get_wb_fine_tune_scaled)"
+
+monochromatic_color="$(get_monochromatic_color)"
 if [[ ! $exif_saturation =~ .*(Acros|B\&W).* ]]; then
 	print_data_line "Color" "$exif_saturation"
+elif [[ -n $monochromatic_color ]]; then
+	print_data_line "Monochromatic Color" "$monochromatic_color"
 fi
-print_data_line "Highlight" "$exif_highlight_tone"
-print_data_line "Shadow" "$exif_shadow_tone"
-print_data_line "Dynamic Range" "$(get_dynamic_range)"
+
+print_data_line "Highlight" "$(apply_sign_fix "$exif_highlight_tone")"
+print_data_line "Shadow" "$(apply_sign_fix "$exif_shadow_tone")"
+
+dynamic_range="$(get_dynamic_range)"
+if [[ -n $dynamic_range ]]; then
+	print_data_line "Dynamic Range" "$dynamic_range"
+elif [[ -n $exif_d_range_priority_auto ]]; then
+	print_data_line "Dynamic Range Priority" "$exif_d_range_priority_auto"
+fi
 print_data_line "Grain Effect" "$(get_grain_effect)"
 print_data_line "Color Chrome Effect" "$exif_color_chrome_effect"
 print_data_line "Color Chrome FX Blue" "$exif_color_chrome_fx_blue"
-print_data_line "Sharpness" "$exif_sharpness"
-print_data_line "Clarity" "$exif_clarity"
-print_data_line "Noise Reduction" "$exif_noise_reduction"
+print_data_line "Sharpness" "$(apply_sign_fix "$exif_sharpness")"
+print_data_line "Clarity" "$(apply_sign_fix "$exif_clarity")"
+print_data_line "Noise Reduction" "$(apply_sign_fix "$exif_noise_reduction")"
 print_end_line
